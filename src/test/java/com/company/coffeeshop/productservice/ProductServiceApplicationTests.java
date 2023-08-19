5a9e92e806c7f2a68fdf6adbf8bd3086a7f287f3
tulio.henrique <tulio.henrique@axians.com>
2023-08-19 15:20:49 +0100

    Initial commit

---
+++ src/test/java/com/company/coffeeshop/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.coffeeshop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
