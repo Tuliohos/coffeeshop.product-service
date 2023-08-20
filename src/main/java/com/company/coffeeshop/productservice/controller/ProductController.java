71081ecaee486c9c55dfda1801bde040fb5589d8
tulio.henrique <tulio.henrique@axians.com>
2023-08-20 21:20:32 +0100

    Configuring SpringDoc & Swagger

--- src/main/java/com/company/coffeeshop/productservice/controller/ProductController.java
+++ src/main/java/com/company/coffeeshop/productservice/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.company.coffeeshop.productservice.controller;
 
 import com.company.coffeeshop.productservice.dto.ProductDTO;
 import com.company.coffeeshop.productservice.service.interfaces.IProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
+@Tag(name = "Product API")
 public class ProductController {
 
     private final IProductService productService;
@@ -29,30 +32,35 @@ public class ProductController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new product")
     public ResponseEntity<Void> createProduct(@RequestBody ProductDTO productDTO) {
         productService.createProduct(productDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a product by ID")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable UUID id) {
         ProductDTO productDTO = productService.getProductById(id);
         return ResponseEntity.ok(productDTO);
     }
 
     @GetMapping
+    @Operation(summary = "Get all products")
     public ResponseEntity<List<ProductDTO>> getAllProducts() {
         List<ProductDTO> products = productService.getAllProducts();
         return ResponseEntity.ok(products);
     }
 
     @PutMapping
+    @Operation(summary = "Update a product")
     public ResponseEntity<Void> updateProduct(@RequestBody ProductDTO productDTO) {
         productService.updateProduct(productDTO);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a product by ID")
     public ResponseEntity<Void> deleteProduct(@PathVariable UUID id) {
         productService.deleteProduct(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
