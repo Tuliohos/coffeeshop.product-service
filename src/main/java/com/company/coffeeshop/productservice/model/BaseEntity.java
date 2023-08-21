21f5eaf28ddda24b3162947d33ef67c1f8dc1f23
tulio.henrique <tulio.henrique@axians.com>
2023-08-21 15:34:24 +0100

    Adding @PrePersist and @PreUpdate to BaseEntity class

--- src/main/java/com/company/coffeeshop/productservice/model/BaseEntity.java
+++ src/main/java/com/company/coffeeshop/productservice/model/BaseEntity.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -23,4 +25,14 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "updated_at")
     private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
 }
